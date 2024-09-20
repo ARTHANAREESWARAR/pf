00794f8faae189fe608c79dab01c2bc387f82dea
ARTHANAREESWARAR <rarthanareeswarar2@gmail..com>
2024-09-20 13:11:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["FrontEnd Developer", "Backend Developer", "Innovator"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+  });
